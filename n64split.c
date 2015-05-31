0b73f95312dcd78ce6fe441772c0ce4b1175e452
Marq <queueRAM@gmail.com>
2015-05-31 09:35:04 -0500

    Lookup segmented addresses in behavior commands 02,04,1C,29,2C

--- n64split.c
+++ n64split.c
@@ -45,6 +45,7 @@ static void print_spaces(FILE *fp, int count)
 
 static void write_behavior(FILE *out, unsigned char *data, rom_config *config, int s)
 {
+   char label[128];
    unsigned int a, i;
    unsigned int len;
    unsigned int val;
@@ -67,21 +68,21 @@ static void write_behavior(FILE *out, unsigned char *data, rom_config *config, i
          }
       }
       switch (data[a]) {
-         case 0x0C:
-         case 0x2A:
          case 0x02:
-         case 0x23:
-         case 0x14:
-         case 0x2F:
          case 0x04:
+         case 0x0C:
+         case 0x14:
+         case 0x23:
          case 0x27:
+         case 0x2A:
+         case 0x2F:
          case 0x37:
             len = 8;
             break;
          case 0x1C:
+         case 0x29:
          case 0x2B:
          case 0x2C:
-         case 0x29:
             len = 12;
             break;
          case 0x30:
@@ -93,18 +94,32 @@ static void write_behavior(FILE *out, unsigned char *data, rom_config *config, i
       }
       val = read_u32_be(&data[a]);
       fprintf(out, ".word 0x%08X", val);
-      // behavior 0x0C is a function pointer
-      if (data[a] == 0x0C) {
-         char label[128];
-         val = read_u32_be(&data[a+4]);
-         fill_addr_label(config, val, label, -1);
-         fprintf(out, ", %s\n", label);
-      } else {
-         for (i = 4; i < len; i += 4) {
-            val = read_u32_be(&data[a+i]);
-            fprintf(out, ", 0x%08X", val);
-         }
-         fprintf(out, "\n");
+      switch(data[a]) {
+         case 0x0C: // behavior 0x0C is a function pointer
+            val = read_u32_be(&data[a+4]);
+            fill_addr_label(config, val, label, -1);
+            fprintf(out, ", %s\n", label);
+            break;
+         case 0x02: // jump to another behavior
+         case 0x04: // jump to segmented address
+         case 0x1C: // sub-objects
+         case 0x29: // sub-objects
+         case 0x2C: // sub-objects
+            for (i = 4; i < len-4; i += 4) {
+               val = read_u32_be(&data[a+i]);
+               fprintf(out, ", 0x%08X", val);
+            }
+            val = read_u32_be(&data[a+len-4]);
+            fill_addr_label(config, val, label, -1);
+            fprintf(out, ", %s\n", label);
+            break;
+         default:
+            for (i = 4; i < len; i += 4) {
+               val = read_u32_be(&data[a+i]);
+               fprintf(out, ", 0x%08X", val);
+            }
+            fprintf(out, "\n");
+            break;
       }
       a += len;
    }
