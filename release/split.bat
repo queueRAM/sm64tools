290ff2a7cfde19be69f52b15766a1bbe387b4774
Marq <queueRAM@gmail.com>
2015-12-27 21:47:21 -0600

    Autodetect config file from Windows batch file

--- release/split.bat
+++ release/split.bat
@@ -1,7 +1,7 @@
 :: change to batch file's directory so tools and config file can be referenced
 pushd %~dp0
-:: split input ROM using verbose mode and specify config file
-tools\n64split.exe -v -c configs\sm64.u.config %1
+:: split input ROM using verbose mode and let it autodetect the config file
+tools\n64split.exe -v %1
 popd
 :: wait for user
 pause
