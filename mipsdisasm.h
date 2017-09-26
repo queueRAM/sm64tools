c169556e16b33c7659bf0f52b0906d4f5752ad84
Marq <queueRAM@gmail.com>
2017-09-26 10:15:46 +0000

    Include API changes in mipsdisasm.h

--- mipsdisasm.h
+++ mipsdisasm.h
@@ -18,20 +18,27 @@ disasm_state *disasm_state_alloc(void);
 // state: disassembler state returned from disasm_state_alloc() or mipsdisasm_pass1()
 void disasm_state_free(disasm_state *state);
 
+// add a label to the disassembler state
+// state: disassembler state returned from disasm_state_alloc() or mipsdisasm_pass1()
+// vaddr: virtual address of label
+// name: string name of label (if NULL, generated based on vaddr)
+void disasm_state_add_label(disasm_state *state, unsigned int vaddr, const char *name);
+
 // first pass of disassembler - collects procedures called and sorts them
 // data: buffer containing raw MIPS assembly
-// datalen: length of 'data'
+// offset: buffer offset to start at
+// data_len: length of 'data'
 // vaddr: virtual address of first byte
 // syntax: assembler syntax to use
 // merge_pseudo: if true, link up pseudo instructions
 // state: disassembler state. if NULL, is allocated, returned at end
-// returns disassembler state collected during first pass
-disasm_state *mipsdisasm_pass1(unsigned char *data, size_t data_len, unsigned int vaddr, asm_syntax syntax, int merge_pseudo, disasm_state *state);
+void mipsdisasm_pass1(unsigned char *data, unsigned int offset, size_t data_len, unsigned int vaddr, disasm_state *state);
 
 // disassemble a region of code, output to file stream
 // out: stream to output data to
 // state: disassembler state from pass1
-void mipsdisasm_pass2(FILE *out, disasm_state *state);
+// offset: starting offset to match in disassembler state
+void mipsdisasm_pass2(FILE *out, disasm_state *state, unsigned int offset);
 
 // get version string of raw disassembler
 const char *disasm_get_version(void);
